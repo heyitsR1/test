5f961826b972f5f3e03a61deaa2b9d8526b532a0
Aarohan <aarohan567@gmail.com>
2025-01-17 10:42:47 +0545

    first commit

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    int age; 
+    //int number (1000); //functional notation declararion and initialization
+    //int balance {100000}; //braced initializer method ; recommended
+
+    cout << "Enter your name: ";
+    getline(cin,name);
+
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << 077;
+
+    cout << "Your name is " << name << " and your age is " << age << endl;
+    return 0;
+}
